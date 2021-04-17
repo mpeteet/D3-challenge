63c15e8dbb2195d47861717e98e42ceb45d594f6
Mona Peteet <mpeteet@comcast.net>
2021-04-16 20:10:54 -0400

    updates

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,24 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 1000;
+var svgHeight = 550;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var svgWidth = 1000;
+var svgHeight = 550;
+
+// Create SVG wrapper 
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+// Appended a SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
